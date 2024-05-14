f12c7a8ea37a9e8dd2e8300adbc0cfdcc34f6a46
nov0id <reddit285035@duck.com>
2024-05-13 20:32:44 -0500

    First python programs commit

---
+++ python/may 2024/main-5132024-2.py
@@ -0,0 +1,11 @@
+#Introduction to python lab in TDX Arena
+#Exploring Conditions
+
+#Grants access based on user input
+
+age = input()
+
+if int(age) > 18:
+    print("Access Granted")
+else:
+    print("Access Denied")
---
+++ python/may 2024/main-5132024.py
@@ -0,0 +1,18 @@
+#Introduction to python lab in TDX Arena
+#Exploring Variables
+
+my_string ='Hello World!'
+print(my_string)
+
+num = 31
+print(num)
+
+number_as_string = '42'
+number = int(number_as_string)
+print(number)
+
+name = 'Ezra'
+age = 25
+print('My name is', name, 'and I am', age, 'years old.')
+
+print(f'My name is {name} and I am {age} years old.')
