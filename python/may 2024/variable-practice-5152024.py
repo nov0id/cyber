cbe3ec2017f851fc6162a6a9411979eb116b7852
nov0id <reddit285035@duck.com>
2024-05-15 19:15:13 -0500

    Updates

--- python/may 2024/variable-practice-5152024.py
+++ python/may 2024/variable-practice-5152024.py
@@ -1,10 +1,10 @@
-#In this lesson we are messing with lists and dictionaries
+#In this lesson we are messing with lists and dictionaries, and loop structures
 
 #initializes the list
 my_list = [1, 2, 3, 4]
 
 #initializes the dictionary
-myDict = {"Name": "nov0id", "Age": 24}
+myDict = {"Name": "nov0id", "Age": 25}
 
 #Does some stuff to manipulate the list and dictionary
 my_list.append(420)
@@ -12,6 +12,8 @@ my_list.append(420)
 my_list.insert(5, "I LOVE CHEESE")
 
 myDict["Can backflip?"] = False
+
+myDict["Can backflip?"] = True
 
 #Removes an item based off of an index
 del my_list[0]
@@ -22,6 +24,16 @@ my_list.remove(3)
 #Removes my "name" key-value pair
 del myDict["Name"]
 
+#Appends '1, 2, 3, 4, 5, 6' to my_list using a for loop
+for i in range(1, 7):
+    my_list.append(i)
+
+x = 0
+while len(myDict) < 15:
+    myDict[x] = x
+    x += 1
+
+#Appends to the dictionary until the dictionary is to a certain length
 #Prints the list
 print(my_list)
 print(myDict)
